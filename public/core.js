4dae18ac55ac83527d40866e6dbe3f5e267f1640
Khalid Nazmus Sakib <knsakib@gmail.com>
2016-09-25 03:58:47 +0000

    little push
    little push

--- public/core.js
+++ public/core.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var appmodule = angular.module('appmodule', []);
 
 appmodule.controller('mainController', function($scope, $http) {
@@ -7,4 +9,36 @@ $http.get('/api/data')
   			$scope.todos = data;
   		});
 
-})
+});
+
+appmodule.controller('socketController', function($scope, socket) {
+
+  socket.on('chat message', function(msg){
+    $scope.data=msg;
+    });
+});
+
+
+appmodule.factory('socket', function ($rootScope) {
+  var socket = io.connect();
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+});
